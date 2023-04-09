ff40beb0ff7a119143e7192b04a3761c1cfeaa1e
carrottown <anabianchini@outlook.com.br>
2023-04-09 20:32:25 -0300

    :bricks: ci: add  time and date to the view

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,27 @@ const input = document.querySelector('form')
 const list = document.querySelector('#list')
 const inputList = document.querySelector("#input-list")
 const addBtn = document.querySelector(".add-task")
+var timeDisplay = document.getElementById("time");
+
+
+function refreshTime() {
+  var dateString = new Date().toLocaleString("en-US", option);
+  var formattedString = dateString.replace(", ", " - ");
+  timeDisplay.innerHTML = formattedString;
+}
+
+const option = {
+    year: 'numeric',
+    month: ('long' || 'short' || 'numeric'),
+    weekday: ('long' || 'short'),
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    era: ('long' || 'short'),
+    timeZoneName: ('long' || 'short')
+}
+
+setInterval(refreshTime, 1000);
 
 function addTask (task) {
     const listItem = document.createElement('li');
@@ -20,7 +41,6 @@ addBtn.addEventListener('click', (e)=> {
         inputList.value = ''
     }
 })
-
 
 list.addEventListener('click', (event)=>{
     if(event.target.tagName === 'BUTTON'){
